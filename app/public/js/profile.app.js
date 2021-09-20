78eac4b18ed584a7b4d1f2c5f86d04813cde7755
pricekj <pricekj@iu.edu>
2021-09-20 10:14:55 -0400

    form and submit

--- app/public/js/profile.app.js
+++ app/public/js/profile.app.js
@@ -6,25 +6,39 @@
              useremail: '',
              userdob: '',
              userage: '',
+             dob:''
+        }
+    },
+    computed: {
+        prettyBirthday() {
+            // return this.dob;
+            return dayjs(this.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then(data =>  {
+                var userdata = data.results[0];
+                this.dob = userdata.dob.date;
+                console.log(userdata)
+                this.username = userdata.name.first + " " + userdata.name.last;
+                this.usercountry = userdata.location.country;
+                this.useremail = userdata.email;
+                this.userdob = userdata.dob.date.split("");
+                this.userage = userdata.dob.age;
+                this.userpic = userdata.picture.large;
+    
+            })
         }
     },
 
     created() {
 
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then(data =>  {
-            var userdata = data.results[0]
-            console.log(userdata)
-            this.username = userdata.name.first + " " + userdata.name.last;
-            this.usercountry = userdata.location.country;
-            this.useremail = userdata.email;
-            this.userdob = userdata.dob.date.split("");
-            this.userage = userdata.dob.age;
-            this.userpic = userdata.picture.large;
+        this.fetchUserData();   
 
-        })
-        }   
-
+        }
     })
 Vue.createApp(Profile).mount('#usern');
